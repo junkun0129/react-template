374d9efa5318cffcd8ae521e4a8acab1fc4d8621
unknown <penpenetai@icloud.com>
2024-04-09 22:48:16 +0900

    init

---
+++ src/store/store.ts
@@ -0,0 +1,35 @@
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+import { testReducer } from "../features/testSlice";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+const persisConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  testReducer,
+});
+
+const persistedReducer = persistReducer(persisConfig, reducer);
+
+export const store = configureStore({
+  reducer: {
+    persistedReducer,
+  }, // Remove the extra 'reducer' field
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat([]),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
